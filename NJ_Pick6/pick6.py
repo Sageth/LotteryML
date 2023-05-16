953f44b41e77ee2aaf3f6de31dfff2851da7344d
Sage Russell <Sageth@users.noreply.github.com>
2023-05-15 23:39:08 -0400

    Revert functions because it now allows duplicates. For another day.

--- NJ_Pick6/pick6.py
+++ NJ_Pick6/pick6.py
@@ -10,23 +10,99 @@ mean_allowance = 0.05
 accuracy_allowance = 0.43
 
 
-def test_linear_regression_model(model, x_test, y_test):
-    """Test the linear regression model and calculate the accuracy."""
-    return model.score(x_test, y_test)
+def predict_and_check():
+    # Calculate the median sum of winning numbers based on the data
+    median_sum = data.iloc[:, 2:].sum(axis=1).median()
 
+    # Create an empty list to store the predicted values for each ball
+    predictions_list = []
+    accuracies = []
 
-def make_predictions(model, x_test):
-    """Make predictions using the trained model."""
-    return model.predict(x_test)
+    # Train a separate model for each ball
+    for ball in range(1, 7):
+        # Split data into X and y
+        x = data.drop(["Date", f"Ball{ball}"], axis=1)
+        y = data[f"Ball{ball}"]
 
+        # Split data into training and testing sets
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5)
 
-def find_mode_values(predictions_df):
-    """Find the mode values for each ball's predictions."""
-    return predictions_df.mode(axis=1)
+        # Train the model
+        model = LinearRegression()
+        model.fit(x_train, y_train)
 
+        # Test the model and calculate accuracy
+        accuracy = model.score(x_test, y_test)
+        accuracies.append(accuracy)
 
-def check_predictions(mode_values, accuracies, mean_allowance, accuracy_allowance):
-    """Check the predictions against the given criteria."""
+        # Make predictions
+        predictions = model.predict(x_test)
+
+        # Append the predicted values to the list
+        predictions_list.append(predictions)
+
+    # Convert the predictions list to a Pandas DataFrame
+    predictions_df = pd.DataFrame(predictions_list).transpose()
+
+    # Find the mode of the predicted values for each ball
+    mode_values = predictions_df.mode(axis=1)
+
+    # Ensure uniqueness for the final ball prediction
+    final_mode_values = mode_values.iloc[-1, :]
+    while len(final_mode_values) != len(np.unique(final_mode_values)):
+        # If duplicates exist, retrain the models for all balls and update the mode values
+        predictions_list = []
+        for ball in range(1, 7):
+            x = data.drop(f"Ball{ball}", axis=1)
+            y = data[f"Ball{ball}"]
+            x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5)
+            model = LinearRegression()
+            model.fit(x_train, y_train)
+            predictions = model.predict(x_test)
+            predictions_list.append(predictions)
+        predictions_df = pd.DataFrame(predictions_list).transpose()
+        mode_values = predictions_df.mode(axis=1)
+
+        # Ensure uniqueness for each ball
+        for ball in range(1, 7):
+            ball_mode_values = mode_values.loc[ball - 1, :].values
+            unique_values, counts = np.unique(ball_mode_values, return_counts=True)
+            duplicate_values = unique_values[counts > 1]
+
+            for duplicate_value in duplicate_values:
+                duplicate_indices = np.where(ball_mode_values == duplicate_value)[0]
+                for index in duplicate_indices:
+                    x = data.drop(f"Ball{ball}", axis=1)
+                    y = data[f"Ball{ball}"]
+                    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5)
+                    model = LinearRegression()
+                    model.fit(x_train, y_train)
+                    predictions = model.predict(x_test)
+                    ball_mode_values[index] = predictions[0]
+
+            mode_values.loc[ball - 1, :] = ball_mode_values
+
+        # Ensure uniqueness for the final ball prediction
+        final_mode_values = mode_values.iloc[-1, :].values
+        unique_final_values, counts = np.unique(final_mode_values, return_counts=True)
+        duplicate_final_values = unique_final_values[counts > 1]
+
+        for duplicate_final_value in duplicate_final_values:
+            duplicate_final_indices = np.where(final_mode_values == duplicate_final_value)[0]
+            for index in duplicate_final_indices:
+                x = data.drop("Ball6", axis=1)
+                y = data["Ball6"]
+                x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5)
+                model = LinearRegression()
+                model.fit(x_train, y_train)
+                predictions = model.predict(x_test)
+                final_mode_values[index] = predictions[0]
+
+        mode_values.iloc[-1, :] = final_mode_values
+
+        final_mode_values = mode_values.iloc[-1, :]
+
+    # Print the mode values and accuracy for each ball
     all_above_threshold = True
     for ball in range(1, 7):
         mode_value = mode_values[ball - 1][0]
@@ -36,9 +112,10 @@ def check_predictions(mode_values, accuracies, mean_allowance, accuracy_allowanc
         if accuracy < accuracy_allowance:
             all_above_threshold = False
 
-    median_sum = data.iloc[:, 2:].sum(axis=1).median()
+    # Calculate the sum of the final ball predictions for balls 1 through 6
     predicted_sum = mode_values.iloc[0, :].sum()
 
+    # Check if the sum of the predicted winning numbers is within 5% of the median sum
     print(f"Median Sum: {median_sum}")
     print(f"Predicted sum: {predicted_sum}")
 
@@ -48,31 +125,7 @@ def check_predictions(mode_values, accuracies, mean_allowance, accuracy_allowanc
     else:
         print(f"The sum of the predicted winning numbers is not within {mean_allowance * 100}% of the median sum "
               f"or all balls do not have accuracy above {accuracy_allowance * 100}%.")
-        predict_and_check()
-
-
-def predict_and_check():
-    """Perform the prediction and checking process."""
-    predictions_list = []
-    accuracies = []
-
-    for ball in range(1, 7):
-        x = data.drop(["Date", f"Ball{ball}"], axis=1)
-        y = data[f"Ball{ball}"]
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5)
-
-        model = LinearRegression()
-        model.fit(x_train, y_train)
-
-        accuracy = test_linear_regression_model(model, x_test, y_test)
-        accuracies.append(accuracy)
-
-        predictions = make_predictions(model, x_test)
-        predictions_list.append(predictions)
-
-    predictions_df = pd.DataFrame(predictions_list).transpose()
-    mode_values = find_mode_values(predictions_df)
-    check_predictions(mode_values, accuracies, mean_allowance, accuracy_allowance)
+        predict_and_check()  # Call the function recursively
 
 
 # Start the prediction and checking process

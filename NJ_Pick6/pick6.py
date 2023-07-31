7c270637ac05646327232cdc30b800005f6527db
Sage Russell <Sageth@users.noreply.github.com>
2023-07-31 16:32:36 -0400

    Easier configuration and changes for tests

--- NJ_Pick6/pick6.py
+++ NJ_Pick6/pick6.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from datetime import datetime, timedelta
 
 sys.setrecursionlimit(50000)
 
@@ -12,9 +13,12 @@ sys.setrecursionlimit(50000)
 csv_files = glob.glob("./*.csv")
 data = pd.concat([pd.read_csv(file) for file in csv_files])
 
+""" Configuration """
 mean_allowance = 0.05
-accuracy_allowance = 0.62
-test_size = 0.0015
+accuracy_allowance = 0.55
+test_size = 0.01
+myrange = range(1, 7)  # 6 balls, indexed 1 - 7
+""" End Configuration """
 
 
 def calculate_mode_of_sums():
@@ -53,6 +57,19 @@ def handle_duplicates(ball_mode_values, ball):
 def predict_and_check():
     print("-----------------------")
 
+    # Define the cutoff date as "relatively recent" (e.g., 3 months ago from the current date)
+    cutoff_date = datetime.now() - timedelta(days=1000)
+
+    # Convert the "Date" column to datetime
+    data["Date"] = pd.to_datetime(data["Date"])
+
+    # Filter data for dates after the cutoff date
+    filtered_data = data[data["Date"] > cutoff_date]
+
+    if filtered_data.empty:
+        print("No future data available for prediction.")
+        return
+
     # Calculate the mode of the sums
     mode_sum = calculate_mode_of_sums()
 
@@ -64,7 +81,7 @@ def predict_and_check():
     all_above_threshold = True
 
     # Train a separate model for each ball
-    for ball in range(1, 7):
+    for ball in myrange:
         # Split data into X and y
         x = data.drop(["Date", f"Ball{ball}"], axis=1)
         y = data[f"Ball{ball}"]
@@ -97,7 +114,7 @@ def predict_and_check():
     mode_values = predictions_df.mode(axis=1)
 
     # Ensure uniqueness for each ball
-    for ball in range(1, 7):
+    for ball in myrange:
         ball_mode_values = mode_values.loc[ball - 1, :].values
         ball_mode_values = handle_duplicates(ball_mode_values, ball)
         mode_values.loc[ball - 1, :] = ball_mode_values
@@ -108,7 +125,7 @@ def predict_and_check():
 
     # Print the predicted values and accuracy for each ball
     print("Predicted values:")
-    for ball in range(1, 7):
+    for ball in myrange:
         rounded_value = round(final_mode_values[ball - 1])
         accuracy_percentage = round(accuracies[ball - 1] * 100, 2)
         print(f"Ball{ball}: {rounded_value}\tAccuracy: {accuracy_percentage}%")
@@ -116,15 +133,14 @@ def predict_and_check():
     # Calculate the sum of the final ball predictions for balls 1 through 6
     predicted_sum = final_mode_values.sum()
 
-    print(f"")
-    print(f"Mode sum: {mode_sum}")
-    print(f"Sum of predicted values: {predicted_sum}")
+    # print(f"")
+    # print(f"Mode sum: {mode_sum}")
+    # print(f"Sum of predicted values: {predicted_sum}")
 
     # Check if the sum of the predicted winning numbers is within 5% of the mode sum
     if abs(predicted_sum - mode_sum) <= mean_allowance * mode_sum and all_above_threshold:
-        print(f"SUCCESS: The sum of the predicted winning numbers is within {mean_allowance * 100}% of the mode sum "
-              f"and all balls meet the accuracy threshold of {accuracy_allowance * 100}%")
-        # print(f"The current date and time is {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+        print(
+            f"SUCCESS!")  # print(f"SUCCESS: The sum of the predicted winning numbers is within {mean_allowance * 100}% of the mode sum "  #       f"and all balls meet the accuracy threshold of {accuracy_allowance * 100}%")  # print(f"The current date and time is {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
     else:
         # print(f"FAILURE:The sum of the predicted winning numbers is not within {mean_allowance * 100}% of the mode "
         #       f"sum or all balls do not have accuracy above {accuracy_allowance * 100}%.")
